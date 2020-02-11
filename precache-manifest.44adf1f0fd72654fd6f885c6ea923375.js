de6d842ef19b9aaafbe3c6344d1212e584d24d1a
aquiladev <aquiladev@users.noreply.github.com>
2020-02-11 11:21:42 +0000

    Automated deployment: Tue Feb 11 11:21:42 UTC 2020 bebdc160f1476d9054ec7e81ae73a7cc61ee79da

---
+++ precache-manifest.44adf1f0fd72654fd6f885c6ea923375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f92fbc20d6d25c7eccd2c179419dd1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63bb3d5a729e36ddcc73",
+    "url": "./static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "40c3d7059d1ce9c067f5",
+    "url": "./static/js/2.4bbca026.chunk.js"
+  },
+  {
+    "revision": "6fbb7aa7dec2f74fdb20fcd0c20be492",
+    "url": "./static/js/2.4bbca026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bb3d5a729e36ddcc73",
+    "url": "./static/js/main.6a9fb665.chunk.js"
+  },
+  {
+    "revision": "37f40108c5e311e475fa",
+    "url": "./static/js/runtime-main.32b701c7.js"
+  }
+]);
