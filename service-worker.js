2b76ba3572bd09050a2f00adeefd849519014f71
aquiladev <aquiladev@users.noreply.github.com>
2020-02-11 11:24:58 +0000

    Automated deployment: Tue Feb 11 11:24:58 UTC 2020 2527887b76fef009b8c228e6857d4b6356ad390e

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.44adf1f0fd72654fd6f885c6ea923375.js"
+  "/precache-manifest.eed6bf10189dfe9e829a48c623a22997.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
